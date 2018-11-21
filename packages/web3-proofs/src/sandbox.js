79952046b42c994a513d3a60c3152d46f878babd
Jorge Izquierdo <jorge@aragon.one>
2018-11-21 08:00:44 +0100

    Scaffold JS lib

---
+++ packages/web3-proofs/src/sandbox.js
@@ -0,0 +1,14 @@
+const Web3 = require('web3')
+const Web3Proofs = require('./index')
+
+const provider = new Web3.providers.WebsocketProvider('ws://localhost:8546')
+const proofs = Web3Proofs(provider)
+
+console.log(proofs)
+
+const run = async () => {
+	const response = await proofs.getProof('0x830ad8ef8b5b1c7f22fde94ddb30a19b2c34c2c8')
+	console.log(response)
+}
+
+run()
