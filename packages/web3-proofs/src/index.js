75344fbbd71d59116893ff16fb38c836311a3dbd
Jorge Izquierdo <jorge@izqui.me>
2018-11-21 08:32:51 +0100

    Refactor web3proofs

--- packages/web3-proofs/src/index.js
+++ packages/web3-proofs/src/index.js
@@ -1,18 +1,29 @@
 const Web3 = require('web3')
 
-module.exports = (provider) => ({
-  getProof: (account, storageKeys = [], blockNumber = 'latest') => {
+class Web3Proofs {
+  constructor (provider) {
+    this.web3 = new Web3(provider)
+  }
+
+  getProof (account, storageKeys = [], blockNumber = 'latest') {
+    return this._jsonRpcSend('eth_getProof', [account, storageKeys, blockNumber])
+  }
+
+  _jsonRpcSend(method, params) {
     const payload = {
-      method: 'eth_getProof',
-      params: [
-        account,
-        storageKeys,
-        blockNumber
-      ],
+      method,
+      params,
       jsonrpc: '2.0',
       id: +new Date(),
     }
 
-    return provider.send(payload)
+    return new Promise((resolve, reject) => {
+      this.web3.currentProvider.send(payload, (err, response) => {
+        if (err) reject(err)
+        resolve(response.result)
+      })
+    })
   }
-})
+}
+
+module.exports = Web3Proofs
