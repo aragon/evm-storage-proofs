5da12d3966d5b4d6ea853b08db64bec1f7870dc2
AlexHerrmann(josojo) <josojo@hotmail.de>
2020-07-06 15:21:34 +0200

    chore: add eth-gas-reporter (#5)

--- packages/evm/truffle-config.js
+++ packages/evm/truffle-config.js
@@ -1,11 +1,14 @@
 module.exports = {
   networks: {
     development: {
-     host: "127.0.0.1",
-     gas: 4e6,
-     gasPrice: 10,
-     port: 8545,
-     network_id: "*"
-    }
-  }
-}
+      host: "127.0.0.1",
+      gas: 4e6,
+      gasPrice: 10,
+      port: 8545,
+      network_id: "*",
+    },
+  },
+  mocha: {
+    reporter: "eth-gas-reporter",
+  },
+};
